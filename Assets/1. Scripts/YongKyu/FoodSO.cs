f67f345ed73bea9000ed046de36bbd925af6b0f3
yeduk3 <yeduk33@gmail.com>
2026-04-29 17:41:48 +0900

    add: sprite image for ingridents

--- Assets/1. Scripts/YongKyu/FoodSO.cs
+++ Assets/1. Scripts/YongKyu/FoodSO.cs
@@ -7,8 +7,10 @@ public class FoodSO : ScriptableObject
     [Header("Food Info")]
     [SerializeField] private string foodName;
     [SerializeField] private GameObject prefab;
+    [SerializeField] private Sprite sprite;
 
     public string FoodName => foodName;
     public GameObject Prefab => prefab;
+    public Sprite Sprite => sprite;
 
 }
