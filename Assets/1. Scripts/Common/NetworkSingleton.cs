27e1731f29813814855b1d5f2e908eb13028f5ad
yeduk3 <yeduk33@gmail.com>
2026-06-06 20:32:01 +0900

    wip: add comments

--- Assets/1. Scripts/Common/NetworkSingleton.cs
+++ Assets/1. Scripts/Common/NetworkSingleton.cs
@@ -1,16 +1,28 @@
-// Owned by MinJun Lee
+// Owned by YongKyu Lee
 using System;
 using Fusion;
 using UnityEngine;
 
+/// <summary>
+/// Singleton for NetworkBehavior
+/// </summary>
+/// <typeparam name="T">Any type to use singleton pattern.</typeparam>
 public abstract class NetworkSingleton<T> : NetworkBehaviour where T : Component
 {
+    /// <summary>
+    /// Let this object don't destroy on scene change if it is true.
+    /// </summary>
     [SerializeField] private bool isDontDestroyOnLoad = false;
-    private static T _instance;
-    private static bool quitting = false;
 
+    private static T _instance;
     public static T Instance => _instance;
 
+    /// <summary>
+    /// It is called on `Spawned` method.
+    /// There are some patterns depending on the Spawned method of singleton managers.
+    /// But there are spawning timing gap so that one may fail to initialized by the manager.
+    /// It forces the order by register other's initializer into this singleton object.
+    /// </summary>
     private static Action initializer;
     public static void BindInitializer(Action i)
     {
@@ -39,10 +51,10 @@ public abstract class NetworkSingleton<T> : NetworkBehaviour where T : Component
         initializer?.Invoke();
         initializer = null;
     }
-    protected virtual void OnApplicationQuit()
-    {
-        quitting = true;
-    }
+    // protected virtual void OnApplicationQuit()
+    // {
+    //     quitting = true;
+    // }
 
     // protected virtual void Initialize()
     // {
--- Assets/1. Scripts/Runtime/Food/photon/AuthorityHandler.cs
+++ Assets/1. Scripts/Runtime/Food/photon/AuthorityHandler.cs
@@ -1,23 +1,53 @@
 // owned by Yongkyu Lee
 using Fusion;
 using UnityEngine;
+using System;
 
+/// <summary>
+/// It is utility class for easy handling the authority handoff process.
+/// </summary>
 public class AuthorityHandler : NetworkBehaviour, IStateAuthorityChanged
 {
+    /// <summary>
+    /// It prevent the duplicated authorization.
+    /// </summary>
     bool isAuthorizing;
-    System.Action onAuthorized;
-    System.Action onNotAuthorized;
+    
+    /// <summary>
+    /// Callback for authorized state.
+    /// </summary>
+    Action onAuthorized;
+
+    /// <summary>
+    /// Callback for not authorized state.
+    /// </summary>
+    Action onNotAuthorized;
 
     /// <summary>
     /// It prevents stealing the state authority (similar to lock).
     /// </summary>
     bool barrier = false;
+    
+    /// <summary>
+    /// Block the new request on state authority.
+    /// </summary>
     public void Barrier() => barrier = true;
+    /// <summary>
+    /// Release the block of requesting state authority.
+    /// </summary>
     public void Unbarrier() => barrier = false;
 
+    /// <summary>
+    /// Tag for this object. (debug)
+    /// </summary>
     private string Tag => $"[Auth/{name}/P{Runner.LocalPlayer.PlayerId}]";
 
-    public void RequestStateAuthority(System.Action onAuthorized, System.Action onNotAuthorized)
+    /// <summary>
+    /// Request the state authority.
+    /// </summary>
+    /// <param name="onAuthorized">Callback for authorizing.</param>
+    /// <param name="onNotAuthorized">Callback for not authorizing.</param>
+    public void RequestStateAuthority(Action onAuthorized, Action onNotAuthorized)
     {
         Debug.Log($"{Tag} RequestStateAuthority called. HasAuth={HasStateAuthority} isAuthorizing={isAuthorizing} currentAuth=P{Object.StateAuthority.PlayerId}");
 
@@ -46,7 +76,11 @@ public class AuthorityHandler : NetworkBehaviour, IStateAuthorityChanged
         RPC_RequestStateAuthority(Object.StateAuthority);
     }
 
-    // rpc info is info of the sender. Runs only on the current state authority (target).
+    /// <summary>
+    /// Request the state authority to specific player.
+    /// </summary>
+    /// <param name="player">Player reference specified when calling this method</param>
+    /// <param name="info">Rpc info is info of the sender. Runs only on the current state authority (target).</param>
     [Rpc(RpcSources.All, RpcTargets.All)]
     private void RPC_RequestStateAuthority([RpcTarget] PlayerRef player, RpcInfo info = default)
     {
@@ -72,6 +106,10 @@ public class AuthorityHandler : NetworkBehaviour, IStateAuthorityChanged
         }
     }
 
+    /// <summary>
+    /// Callback for requesting player when successed to get authority.
+    /// </summary>
+    /// <param name="player">In this case, the player is requester.</param>
     [Rpc(RpcSources.All, RpcTargets.All)]
     private void RPC_Authorized([RpcTarget] PlayerRef player)
     {
@@ -79,7 +117,11 @@ public class AuthorityHandler : NetworkBehaviour, IStateAuthorityChanged
         Object.RequestStateAuthority(); // request and the result will be executed on StateAuthorityChanged
     }
 
-
+    /// <summary>
+    /// Callback for requesting player when failed to get authority.
+    /// </summary>
+    /// <param name="player">In this case, the player is requester.</param>
+    /// <param name="isBarriered">Whether it fails because of the barrier?</param>
 	[Rpc(RpcSources.All, RpcTargets.All)]
 	private void Rpc_NotAuthorized([RpcTarget] PlayerRef player, bool isBarriered)
 	{
@@ -94,6 +136,9 @@ public class AuthorityHandler : NetworkBehaviour, IStateAuthorityChanged
         }
 	}
 
+    /// <summary>
+    /// Callback for success of changing state authority.
+    /// </summary>
     public void StateAuthorityChanged()
     {
         Debug.Log($"{Tag} StateAuthorityChanged fired. HasAuth={HasStateAuthority} isAuthorizing={isAuthorizing} newAuth=P{Object.StateAuthority.PlayerId}");
